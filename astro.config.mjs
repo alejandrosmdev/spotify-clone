8050b2ea5506c1e090b7ac653271d7d2f7073e5f
alejandrosmdev <166904357+alejandrosmdev@users.noreply.github.com>
2024-09-07 16:42:59 +0200

    Last details

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte(), react()],
+  output: 'server',
+  adapter: vercel()
 });
